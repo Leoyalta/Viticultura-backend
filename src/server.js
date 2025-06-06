442d63cd809c8140a164117903a4706a4699235a
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-06-06 11:33:21 +0200

    add-crud&error-handling

--- src/server.js
+++ src/server.js
@@ -1,59 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-// import pino from 'pino-http';
+// import logger from './middlewares/logger.js';
 import { env } from './utils/env.js';
 
-import * as productsService from './services/product.js';
+import productsRouter from './routers/products.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 export const startServer = () => {
   const app = express();
-  // const logger = pino({
-  //   transport: {
-  //     target: 'pino-pretty',
-  //   },
-  // });
 
   // app.use(logger);
+
   app.use(cors());
   app.use(express.json());
 
-  app.get('/products', async (req, res) => {
-    const productsData = await productsService.getAllMovies();
+  app.use('/products', productsRouter);
 
-    res.json({
-      status: 200,
-      message: 'Successfully founded products',
-      productsData,
-    });
-  });
-
-  app.get('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await productsService.getMovie(id);
-
-    if (!product) {
-      res.status(404).json({
-        message: `Product with id:${id} is not exist`,
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Product with id:${id} is found`,
-      product,
-    });
-  });
-
-  app.use((req, res) => {
-    res.status(404).json({
-      message: `${req.url} rout is not found.`,
-    });
-  });
-  app.use((error, req, res, next) => {
-    res.status(500).json({
-      message: error.message,
-    });
-  });
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
   const port = Number(env('PORT', 3030));
   app.listen(port, () => {
