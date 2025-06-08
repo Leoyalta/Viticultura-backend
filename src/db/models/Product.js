8016bd7efb41c63ddfd3dda6dc8b7a7d1c014d96
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-06-08 12:40:09 +0200

    add-joi&mongoose-validation

--- src/db/models/Product.js
+++ src/db/models/Product.js
@@ -1,46 +1,61 @@
 import { Schema, model } from 'mongoose';
+import {
+  varietyList,
+  pieVarietyList,
+  codeRegexp,
+} from '../../constants/products.js';
+import { handleServerError, setUpdateOptions } from './mongooseHooks.js';
 
 const productSchema = new Schema(
   {
-    variedad: {
+    code: {
       type: String,
-      enum: [
-        'Macabeu',
-        'Xarello',
-        'Parellada',
-        'Tempranillo',
-        'Garnacha',
-        'Chardonnay',
-        'Verdejo',
-        'Sauvignon Blanc',
-      ],
-      required: true,
+      required: [true, 'El campo "code" es obligatorio.'],
+      validate: {
+        validator: (v) => codeRegexp.test(v),
+        message:
+          'El campo "code" debe tener el formato XX-XX-XX o XXX-XXX-XXX (por ejemplo: 01-02-03 o 100-20-333).',
+      },
+    },
+    variety: {
+      type: String,
+      enum: {
+        values: varietyList,
+        message: 'El campo "variety" debe ser uno de los valores permitidos.',
+      },
+      required: [true, 'El campo "variety" es obligatorio.'],
     },
     clon: {
       type: String,
-      required: true,
+      required: [true, 'El campo "clon" es obligatorio.'],
     },
     pie: {
       type: String,
-      required: true,
+      enum: {
+        values: pieVarietyList,
+        message: 'El campo "pie" debe ser uno de los valores permitidos.',
+      },
+      required: [true, 'El campo "pie" es obligatorio.'],
     },
-
     clonPie: {
       type: String,
-      required: true,
+      required: [true, 'El campo "clonPie" es obligatorio.'],
     },
     total: {
       type: Number,
-      reqired: true,
+      required: [true, 'El campo "total" es obligatorio.'],
     },
     isAvailable: {
       type: Boolean,
-      required: true,
+      required: [true, 'El campo "isAvailable" es obligatorio.'],
     },
   },
   { versionKey: false, timestamps: true },
 );
 
+productSchema.post('save', handleServerError); //this  is a mongoose hook, just to remember:)
+productSchema.pre('findOneAndUpdate', setUpdateOptions);
+productSchema.post('findOneAndUpdate', handleServerError);
 const ProductCollection = model('product', productSchema);
 
 export default ProductCollection;
